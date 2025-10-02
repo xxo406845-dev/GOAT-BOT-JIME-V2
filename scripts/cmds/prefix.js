e9189124fcad8234c1bed6b53f3683dce82d5dc9
NeoKEX <botofnexxo@gmail.com>
2025-10-03 01:40:44 +0600

    Update prefix.js

--- scripts/cmds/prefix.js
+++ scripts/cmds/prefix.js
@@ -45,7 +45,7 @@ module.exports = {
 			confirmThisThread: "Please react to this message to confirm change prefix in your box chat",
 			successGlobal: "Changed prefix of system bot to: %1",
 			successThisThread: "Changed prefix in your box chat to: %1",
-			myPrefix: "Hey senpai! ~_~\n🌐Global prefix is: %1\n🛸Your group chat prefix is: %2"
+			myPrefix: "Hey senpai! ~_~\n🌐Global prefix: %1\n🛸Your group chat prefix: %2"
 		}
 	},
 
